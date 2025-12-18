cbefd02801210b0014e9a653ffc0f9100841a6ad
bala-amypo <bala@amypo.in>
2025-12-18 04:49:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/DemandForecast.java
+++ src/main/java/com/example/demo/entity/DemandForecast.java
@@ -27,4 +27,57 @@ public class DemandForecast {
     private Double confidenceScore;
 
 
+    public Long getId() { 
+        return id; 
+    }
+    public void setId(Long id) { 
+        this.id = id; 
+    }
+
+    public Product getProduct() { 
+        return product; 
+    }
+    public void setProduct(Product product) { 
+        this.product = product; 
+    }
+
+    public Store getStore() {   
+        return store; 
+    }
+    public void setStore(Store store) { 
+        this.store = store; 
+    }
+
+    public LocalDate getForecastDate() { 
+        return forecastDate; 
+    }
+    public void setForecastDate(LocalDate forecastDate) {
+        this.forecastDate = forecastDate;
+    }
+
+    public Integer getPredictedDemand() { 
+        return predictedDemand; 
+    }
+    public void setPredictedDemand(Integer predictedDemand) {
+        this.predictedDemand = predictedDemand;
+    }
+
+    public Double getConfidenceScore() { 
+        return confidenceScore; 
+    }
+    public void setConfidenceScore(Double confidenceScore) {
+        this.confidenceScore = confidenceScore;
+    }
+
+    public DemandForecast(Product product, Store store, LocalDate forecastDate,Integer predictedDemand, Double confidenceScore) {
+        this.product = product;
+        this.store = store;
+        this.forecastDate = forecastDate;
+        this.predictedDemand = predictedDemand;
+        this.confidenceScore = confidenceScore;
+    }
+    
+    public DemandForecast() {
+    
+    }
 }
--- src/main/java/com/example/demo/entity/TransferSuggestion.java
+++ src/main/java/com/example/demo/entity/TransferSuggestion.java
@@ -24,13 +24,70 @@ public class TransferSuggestion {
 
     private Integer quantity;
 
-    @Column(nullable = false)
     private String priority; // HIGH / MEDIUM / LOW
 
-    @Column(nullable = false)
     private String status = "PENDING";
 
     private LocalDateTime suggestedAt = LocalDateTime.now();
 
+    // Constructors
+    }
 
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Store getSourceStore() { return sourceStore; }
+    public void setSourceStore(Store sourceStore) {
+        this.sourceStore = sourceStore;
+    }
+
+    public Store getTargetStore() { return targetStore; }
+    public void setTargetStore(Store targetStore) {
+        this.targetStore = targetStore;
+    }
+
+    public Product getProduct() { return product; }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() { return quantity; }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPriority() { return priority; }
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getStatus() { 
+        return status; 
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getSuggestedAt() { 
+        return suggestedAt; 
+    }
+    public void setSuggestedAt(LocalDateTime suggestedAt) {
+        this.suggestedAt = suggestedAt;
+    }
+
+
+    public TransferSuggestion(Store sourceStore, Store targetStore,
+                              Product product, Integer quantity, String priority) {
+        this.sourceStore = sourceStore;
+        this.targetStore = targetStore;
+        this.product = product;
+        this.quantity = quantity;
+        this.priority = priority;
+    
+    
+    public TransferSuggestion() {
+
+
+    }
 }
