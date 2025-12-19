070813810a87136c9ac2328532b3417b6c10827c
bala-amypo <bala@amypo.in>
2025-12-19 08:17:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/StoreRepository.java
+++ src/main/java/com/example/demo/repository/StoreRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
-    // JpaRepository already provides CRUD methods
+   
 }
