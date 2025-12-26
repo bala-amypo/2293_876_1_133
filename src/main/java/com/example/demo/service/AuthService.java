f7a4a837400ee23d08b98da83dfed219fc985f41
bala-amypo <bala@amypo.in>
2025-12-26 06:54:26 +0000

    User commit

--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -1,9 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.AuthRequestDto;
 import com.example.demo.dto.RegisterRequestDto;
 
 public interface AuthService {
 
     String register(RegisterRequestDto dto);
 
+    // ✅ REQUIRED FOR TEST + CONTROLLER
+    String login(AuthRequestDto dto);
 }
