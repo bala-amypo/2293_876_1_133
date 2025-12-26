7ca46733ce8fb28c5b98ab35e000a386f8744c51
bala-amypo <bala@amypo.in>
2025-12-26 07:09:16 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequestDto.java
+++ src/main/java/com/example/demo/dto/AuthRequestDto.java
@@ -1,30 +1,9 @@
-package com.example.demo.dto;
+package com.example.demo.service;
 
-public class AuthRequestDto {
+import com.example.demo.dto.AuthRequestDto;
 
-    private String username;
-    private String password;
+public interface AuthService {
 
-    public AuthRequestDto() {}
+    String login(AuthRequestDto request);
 
-    public AuthRequestDto(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
