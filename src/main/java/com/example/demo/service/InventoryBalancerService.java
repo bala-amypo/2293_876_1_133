7571e6e27709f65968e0b18ff774409fb8137f8f
bala-amypo <bala@amypo.in>
2025-12-26 19:16:21 +0000

    User commit

--- src/main/java/com/example/demo/service/InventoryBalancerService.java
+++ src/main/java/com/example/demo/service/InventoryBalancerService.java
@@ -1,8 +1,10 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 public interface InventoryBalancerService {
 
-    String balanceInventory();
+    void balanceInventory();
 
-    String getSuggestionById(Long id);
+    List<String> generateSuggestions(Long productId);
 }
