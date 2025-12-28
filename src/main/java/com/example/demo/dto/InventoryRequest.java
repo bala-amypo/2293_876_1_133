34a94d0cfc1ec736251e0506001949da3ab9e02c
bala-amypo <bala@amypo.in>
2025-12-28 15:38:18 +0000

    User commit

--- src/main/java/com/example/demo/dto/InventoryRequest.java
+++ src/main/java/com/example/demo/dto/InventoryRequest.java
@@ -1,32 +1,32 @@
-package com.example.demo.dto;
+// package com.example.demo.dto;
 
-public class InventoryRequest {
+// public class InventoryRequest {
 
-    private Long storeId;
-    private Long productId;
-    private int quantity;
+//     private Long storeId;
+//     private Long productId;
+//     private int quantity;
 
-    public Long getStoreId() {
-        return storeId;
-    }
+//     public Long getStoreId() {
+//         return storeId;
+//     }
 
-    public void setStoreId(Long storeId) {
-        this.storeId = storeId;
-    }
+//     public void setStoreId(Long storeId) {
+//         this.storeId = storeId;
+//     }
 
-    public Long getProductId() {
-        return productId;
-    }
+//     public Long getProductId() {
+//         return productId;
+//     }
 
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
+//     public void setProductId(Long productId) {
+//         this.productId = productId;
+//     }
 
-    public int getQuantity() {
-        return quantity;
-    }
+//     public int getQuantity() {
+//         return quantity;
+//     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-}
+//     public void setQuantity(int quantity) {
+//         this.quantity = quantity;
+//     }
+// }
